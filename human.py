65dc39de3a8356a29490bc79b4c2bd6a9cb64abe
LizzieDworsky <amydworsky15@gmail.com>
2022-05-16 14:21:47 -0600

    updated and tested the human class

--- human.py
+++ human.py
@@ -5,6 +5,12 @@ class Human(Player):
         super().__init__()
 
     def choose_gesture(self):
-        user_selection = int(input('Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 for Lizard, 4 for Spock: '))
-        self.chosen_gesture = self.all_gestures[user_selection]
-        print(f'You chose {self.chosen_gesture}')
+        input_valid = False
+        while input_valid == False:
+            user_selection = input("Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 for Lizard, 4 for Spock: ")
+            if user_selection == "0" or user_selection == "1" or user_selection == "2" or user_selection == "3" or user_selection == "4":
+                user_selection = int(user_selection)
+                self.chosen_gesture = self.all_gestures[user_selection]
+                input_valid = True
+            else:
+                user_selection = print ("Sorry we didn't get that. Please try again.")
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 """
 (5 points): As a developer, I want to make at least 10 commits with descriptive messages.
 
-(15 points): As a developer, I want to find a way to properly incorporate inheritance into my game.
-
 (5 points): As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary.
 
 (10 points): As a developer, I want to store all of the gesture options/choices in a List (AS STRINGS). I want to find a way to utilize the list of gestures within my code (display gesture options, assign player a gesture, etc).
